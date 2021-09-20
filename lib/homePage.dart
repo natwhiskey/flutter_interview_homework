36dbf72d69a7f3cea6466cba1c5cdf52cbecf8ad
natwhiskey <kimngza@hotmail.com>
2021-09-20 15:19:02 +0700

    create button route page

---
+++ lib/homePage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/page/firstPage.dart';
+import 'package:flutter_application/page/SecondPage.dart';
+import 'package:flutter_application/page/ThirthPage.dart';
+import 'package:flutter_application/page/FourthPage.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstPage()),
+                );
+              },
+              child: Text(
+                "Page 1",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: TextButton.styleFrom(backgroundColor: Colors.blue),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: Text(
+                "Page 2",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: TextButton.styleFrom(backgroundColor: Colors.blue),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThirthPage()),
+                );
+              },
+              child: Text(
+                "Page 3",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FourthPage()),
+                );
+              },
+              child: Text(
+                "Page 4",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
